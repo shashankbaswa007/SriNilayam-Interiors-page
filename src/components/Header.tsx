94e652a6f600161380aefbbe9ad027a39a5d7961
BASWA SHASHANK SAI SRI <baswashashank123@gmail.com>
2025-09-30 18:42:22 +0530

    Update website logo in header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+@@ .. @@
+             {/* Logo */}
+             <Link to="/" className="flex items-center space-x-3">
++              <img 
++                src="/Screenshot 2025-06-25 at 10.01.56 AM copy copy.png" 
++                alt="SriNilayam Interiors" 
++                className="h-12 w-auto"
++              />
+             </Link>
