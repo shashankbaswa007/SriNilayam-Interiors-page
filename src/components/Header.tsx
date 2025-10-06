d995324cfc14aab69951f8b8c1712ae862cf6a1c
BASWA SHASHANK SAI SRI <baswashashank123@gmail.com>
2025-10-06 20:40:59 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -77,14 +77,6 @@ const Header = () => {
               </span>
             </Link>
 
-            </Link>
-
-            {/* Desktop Navigation */}
-            <div className="hidden md:flex items-center space-x-8">
-              {navigation.map((item) => (
-                <Link
-                  key={item.name}
-                  to={item.href}
             {/* Desktop Navigation */}
             <div className="hidden md:flex items-center space-x-8">
               {navigation.map((item) => (
